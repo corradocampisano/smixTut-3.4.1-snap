ea970b85cd6154ef7b4a23e524a9756eed59a87b
corradocampisano <corrado.campisano@gmail.com>
2017-11-17 13:15:11 +0100

    new version with both pub and sub, plus the feature

--- pub/src/main/java/eu/servicemix/uaal/tutorials/contextPubSubTutorial/pubBundle/Activator.java
+++ pub/src/main/java/eu/servicemix/uaal/tutorials/contextPubSubTutorial/pubBundle/Activator.java
@@ -15,16 +15,19 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
+    
+    Derived/forked work Copyright 2017-2021 Corrado Campisano
+    
  */
-package org.universAAL.tutorials.context.bus.publisher;
+package eu.servicemix.uaal.tutorials.contextPubSubTutorial.pubBundle;
 
 import org.universAAL.middleware.container.ModuleActivator;
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.context.ContextEvent;
 import org.universAAL.middleware.context.ContextPublisher;
-import org.universAAL.middleware.context.DefaultContextPublisher;
 import org.universAAL.middleware.context.owl.ContextProvider;
 import org.universAAL.middleware.context.owl.ContextProviderType;
+import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.ontology.device.LightActuator;
 import org.universAAL.ontology.device.ValueDevice;
 
@@ -35,34 +38,27 @@ import org.universAAL.ontology.device.ValueDevice;
  * In our case, we simply want to publish a context event, so we do this
  * directly in the start method.
  * 
- * @author Carsten Stockloew
+ * @author Carsten Stockloew, Corrado Campisano
  */
 public class Activator implements ModuleActivator {
 
-	public void start(ModuleContext mc) throws Exception {
+	ContextPublisher contextPublisher = null;
+		
+	public void start(ModuleContext moduleContext) throws Exception {
+		
 		// Set up the context publisher by providing some information about
 		// ourselves. Especially, we need a URI that uniquely identifies this
 		// component and the provider type (one of: controller, gauge,
 		// reasoner).
-		ContextProvider provInfo = new ContextProvider("urn:org.universAAL.tutorial:tut.provider");
-		provInfo.setType(ContextProviderType.controller);
-		ContextPublisher cp = new DefaultContextPublisher(mc, provInfo);
+		ContextProvider contextProvider = new ContextProvider("urn:eu.servicemix.uaal.tutorials:contextPublisher");
+		contextProvider.setType(ContextProviderType.controller);
+		contextPublisher = new MyContextPublisher(moduleContext, contextProvider);
 
-		// Create a context event telling the system that the brightness of a
-		// light source has changed. According to the device ontology the event
-		// describes a triple of the form:
-		// LightActuator hasValue x
-		// In this example, the brightness of the kitchen light was dimmed to
-		// 100% (= the kitchen light was turned on)
-		ContextEvent evt = new ContextEvent(new LightActuator("urn:org.universAAL.aal_space:KitchenLight"),
-				ValueDevice.PROP_HAS_VALUE, 100);
-		cp.publish(evt);
-
-		// Close our context publisher and free all resources. The publisher
-		// should be re-used if multiple events need to be published.
-		cp.close();
 	}
 
-	public void stop(ModuleContext arg0) throws Exception {
+	public void stop(ModuleContext arg0) throws Exception {		
+		// Close our context publisher and free all resources. The publisher
+		// should be re-used if multiple events need to be published.
+		contextPublisher.close();
 	}
 }

d4b3e1bf332c7fb9f8ad0f85d804d9d0383e1608
corradocampisano <corrado.campisano@gmail.com>
2017-12-01 19:54:20 +0100

    working on filePollerTutorial using Camel

---
+++ filePollerTutorial/bundle/src/main/java/eu/servicemix/uaal/tutorials/filePollerTutorial/bundle/Activator.java
@@ -0,0 +1,130 @@
+/*
+    Copyright 2016-2020 Carsten Stockloew
+
+    See the NOTICE file distributed with this work for additional
+    information regarding copyright ownership
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+package eu.servicemix.uaal.tutorials.filePollerTutorial.bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.component.file.FileComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.context.ContextEvent;
+import org.universAAL.middleware.context.ContextPublisher;
+import org.universAAL.middleware.context.DefaultContextPublisher;
+import org.universAAL.middleware.context.owl.ContextProvider;
+import org.universAAL.middleware.context.owl.ContextProviderType;
+import eu.servicemix.uaal.tutorials.docArchOntologyTutorial.ontology.Document;
+
+/**
+ * The module activator handles the starting and stopping of this module. It is
+ * similar to the OSGi BundleActivator.
+ * 
+ * In our case, we simply want to publish a context event, so we do this
+ * directly in the start method.
+ * 
+ * @author Carsten Stockloew
+ */
+public class Activator implements ModuleActivator {
+
+	ContextPublisher contextPublisher = null;
+	CamelContext camelContext = null;
+
+	public void start(ModuleContext mc) throws Exception {
+
+		System.out.println("STARTING : " + this.getClass().getPackage());
+		
+		setupContextPublisher(mc);
+		
+		setupCamelContext();
+		camelContext.start();	
+	}
+
+	public void stop(ModuleContext arg0) throws Exception {
+		System.out.println("STOPPING : " + this.getClass().getPackage());
+
+		// Close our context publisher and free all resources. The publisher
+		// should be re-used if multiple events need to be published.
+		if (contextPublisher != null)
+			contextPublisher.close();
+		
+		if (camelContext != null)
+			camelContext.stop();
+
+	}
+
+	private void setupContextPublisher(ModuleContext mc) {
+		// Set up the context publisher by providing some information about
+		// ourselves. Especially, we need a URI that uniquely identifies this
+		// component and the provider type (one of: controller, gauge,
+		// reasoner).
+		ContextProvider provInfo = new ContextProvider("urn:eu.servicemix.uaal.tutorials.filePollerTutorial:pubBundle");
+		provInfo.setType(ContextProviderType.controller);
+		contextPublisher = new DefaultContextPublisher(mc, provInfo);
+	}
+
+	private void setupCamelContext() {
+		camelContext = new DefaultCamelContext();
+		Component filePoller = new FileComponent();
+		camelContext.addComponent("incomingFiles", filePoller);
+		
+		Component fileSender = new FileComponent();
+		camelContext.addComponent("processedFiles", fileSender);
+		
+		if (3+2==5) {
+			sendEvent();
+		}
+		
+	}
+
+	private void sendEvent() {
+
+		// Create a context event telling the system that the brightness of a
+		// light source has changed. According to the device ontology the event
+		// describes a triple of the form:
+		// LightActuator hasValue x
+		// In this example, the brightness of the kitchen light was dimmed to
+		// 100% (= the kitchen light was turned on)
+		/*
+		 * ContextEvent evt = new ContextEvent(new
+		 * LightActuator("urn:org.universAAL.aal_space:KitchenLight"),
+		 * ValueDevice.PROP_HAS_VALUE, 100);
+		 */
+
+		Document document = new Document("urn:eu.servicemix.uaal.tutorials.filePollerTutorial:MokedDocumentHashMap");
+		document.setDocumentURL("nowhere");
+		document.setDocumentMimetype("pdf");
+		document.setDocumentMd5("123456789012345678900123456789012345678901234567890012345678901234");
+
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd_hh:mm:ss");
+		Date now = new Date();
+		String ts = sdf.format(now);
+
+		document.setImportTimestamp(ts);
+
+		String predicate = "mockDocImportedOn";
+		ContextEvent evt = new ContextEvent(document, predicate, ts);
+
+		System.out.println("SENDING EVENT : " + evt.toString());
+
+		contextPublisher.publish(evt);
+	}
+}
---
+++ filePollerTutorial/readme.md
@@ -0,0 +1,114 @@
+
+# contextPubSubTutorial for uAAL 3.4.1-snapshot
+
+
+
+
+
+## general setup and build instructions
+
+
+see parent (root) project readme for instructions.
+
+those are a prerequisite to run any of the tutorials.
+
+
+
+
+
+## running this tutorial
+
+
+first make sure you can setup the environment correctly, see previous section.
+
+once done, see the following for deployment instructions. 
+
+
+
+### 0) Install Camel into distro.karaf:
+
+
+add the feature repository for Camel this way:
+
+	karaf@uAAL>feature:repo-add mvn:org.apache.camel.karaf/apache-camel/2.9.0/xml/features
+	Adding feature url mvn:org.apache.camel.karaf/apache-camel/2.9.0/xml/features
+
+
+double check:
+
+	karaf@uAAL>feature:list | grep -i camel
+	xml-specs-api                                   | 1.9.0            |           | camel-2.9.0                       |  
+	camel                                           | 2.9.0            |           | camel-2.9.0                       |   
+	camel-core                                      | 2.9.0            |           | camel-2.9.0                       |   
+	...
+	camel-zookeeper                                 | 2.9.0            |           | camel-2.9.0                       |   
+
+once the Camel feature repo is configured, the tutorial feature can reference it.
+
+
+
+
+
+### 1) Add feature repository:
+
+
+since the feature got installed locally (mvn: = maven local repo):
+
+	[INFO] Installing /home/corrado/uaal/smixTut-3.4.1-snap/filePollerTutorial/feature/target/features/features.xml to /home/corrado/.m2/repository/eu/servicemix/uaal/tutorials/filePollerTutorial.feature/3.4.1-SNAPSHOT/filePollerTutorial.feature-3.4.1-SNAPSHOT-features.xml
+
+
+we can add the local (mvn: = maven local repo) feature-repo in Karaf:
+
+	karaf@uAAL>feature:repo-add mvn:eu.servicemix.uaal.tutorials/filePollerTutorial.feature/3.4.1-SNAPSHOT/xml/features
+	Adding feature url mvn:eu.servicemix.uaal.tutorials/filePollerTutorial.feature/3.4.1-SNAPSHOT/xml/features
+
+
+double check:
+
+	karaf@uAAL>feature:repo-list | grep -i filepoll
+	uaalTutorials-Feature             | mvn:eu.servicemix.uaal.tutorials/filePollerTutorial.feature/3.4.1-SNAPSHOT/xml/features
+
+
+
+
+
+### 2) install feature:
+
+add "-v" (verbose) to let it show more:
+
+	karaf@uAAL>feature:install -v uaalTuts-filePollerTutorial
+	Installing feature uaalTuts-filePollerTutorial 3.4.1-SNAPSHOT
+	Installing feature camel-core 2.18.4
+	Installing feature xml-specs-api 2.7.0
+	Installing bundle mvn:org.apache.servicemix.specs/org.apache.servicemix.specs.activation-api-1.1/2.7.0
+	Installing bundle mvn:org.apache.servicemix.specs/org.apache.servicemix.specs.stax-api-1.0/2.7.0
+	Installing bundle mvn:org.apache.servicemix.specs/org.apache.servicemix.specs.jaxb-api-2.2/2.7.0
+	Installing bundle mvn:org.codehaus.woodstox/stax2-api/3.1.4
+	Installing bundle mvn:org.codehaus.woodstox/woodstox-core-asl/4.4.1
+	Installing bundle mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jaxb-impl/2.2.11_1
+	Installing bundle mvn:org.apache.camel/camel-core/2.18.4
+	Installing bundle mvn:org.apache.camel/camel-catalog/2.18.4
+	Installing bundle mvn:org.apache.camel/camel-commands-core/2.18.4
+	Installing feature uaalTuts-docArchOntologyTutorial 3.4.1-SNAPSHOT
+	Installing bundle mvn:eu.servicemix.uaal.tutorials/docArchOntologyTutorial.bundle/3.4.1-SNAPSHOT
+	Installing bundle mvn:eu.servicemix.uaal.tutorials/filePollerTutorial.bundle/3.4.1-SNAPSHOT
+	STARTING : package eu.servicemix.uaal.tutorials.filePollerTutorial.bundle
+	SENDING EVENT : urn:org.universAAL.middleware.context.rdf:ContextEvent#_:7f000101fefe5560:bb
+
+
+
+WHAT'S BELOW SHALL BE UPDATED
+
+double check:
+
+	karaf@uAAL>list | grep -i pubsub
+	101 | Active |  80 | 3.4.1.SNAPSHOT | universAAL Tutorials Context Bus PubSub - Subscriber Bundle 
+	102 | Active |  80 | 3.4.1.SNAPSHOT | universAAL Tutorials Context Bus PubSub - Publisher Bundle     
+
+
+when closing karaf, you should see these messages:
+
+	karaf@uAAL>^D
+	...
+	STOPPING : package eu.servicemix.uaal.tutorials.contextPubSubTutorial.pubBundle
+	STOPPING : package eu.servicemix.uaal.tutorials.contextPubSubTutorial.subBundle
